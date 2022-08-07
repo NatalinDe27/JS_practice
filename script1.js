7d5ac69ab2b889130cf1526395cda0b13f2c646a
NatalinDe27 <nnnnnnnbnv@gmail.com>
2022-08-07 22:02:53 +0300

    add names of let, string, array

---
+++ script1.js
@@ -0,0 +1,21 @@
+let name = 'Nataliia';
+name = 'Hanna';
+const studentName = 'Nataliia';
+
+let userName;
+let topUserName;
+let activeUsersNameList;
+let activeUserName;
+
+let numbers = [1, 255, 378];
+let numbers1 = [466, 285, 9768];
+
+// объект с произвольными ключами и значениями
+
+let world = 'world';
+let str = `Hello ${world}`;
+
+
+
+
+
